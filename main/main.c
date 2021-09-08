07c8af6db939c5ee9f8b5dea527a892289b29909
andrea.lambruschini <andrea.lambruschini@telecomitalia.it>
2021-09-08 12:18:18 +0200

    Aggiunto flash su nvs dei range stick RC

--- main/main.c
+++ main/main.c
@@ -1,5 +1,8 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include <esp_system.h>
+#include <nvs_flash.h>
+#include <nvs.h>
 #include "driver/rmt.h"
 #include "driver/gpio.h"
 #include "rc.h"
@@ -12,6 +15,57 @@
 #define RC_BUFF_BLOCK_NUM     4                  /* Memory Blocks */
 #define RC_TICK_TRASH         100                /* Interference */
 
+
+
+esp_err_t my_rc_init_flash(){
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        // NVS partition was truncated and needs to be erased
+        // Retry nvs_flash_init
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        err = nvs_flash_init();
+    }
+	return err;
+}
+esp_err_t my_rc_write_flash(rc_handle_t rc_handle){
+    nvs_handle_t my_handle;
+    ESP_ERROR_CHECK(nvs_open("RC_CAL", NVS_READWRITE, &my_handle));
+    ESP_ERROR_CHECK(nvs_set_u8(my_handle, "FLASHED", 1));
+
+    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MIN_1", rc_handle->rc_stick_ranges[0].min));
+    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MIN_2", rc_handle->rc_stick_ranges[1].min));
+    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MIN_3", rc_handle->rc_stick_ranges[2].min));
+    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MIN_4", rc_handle->rc_stick_ranges[3].min));
+    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MIN_5", rc_handle->rc_stick_ranges[4].min));
+    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MIN_6", rc_handle->rc_stick_ranges[5].min));
+    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MIN_7", rc_handle->rc_stick_ranges[6].min));
+    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MIN_8", rc_handle->rc_stick_ranges[7].min));
+
+    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MAX_1", rc_handle->rc_stick_ranges[0].max));
+    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MAX_2", rc_handle->rc_stick_ranges[1].max));
+    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MAX_3", rc_handle->rc_stick_ranges[2].max));
+    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MAX_4", rc_handle->rc_stick_ranges[3].max));
+    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MAX_5", rc_handle->rc_stick_ranges[4].max));
+    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MAX_6", rc_handle->rc_stick_ranges[5].max));
+    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MAX_7", rc_handle->rc_stick_ranges[6].max));
+    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MAX_8", rc_handle->rc_stick_ranges[7].max));
+
+    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_CNT_1", rc_handle->rc_stick_ranges[0].center));
+    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_CNT_2", rc_handle->rc_stick_ranges[1].center));
+    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_CNT_3", rc_handle->rc_stick_ranges[2].center));
+    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_CNT_4", rc_handle->rc_stick_ranges[3].center));
+    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_CNT_5", rc_handle->rc_stick_ranges[4].center));
+    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_CNT_6", rc_handle->rc_stick_ranges[5].center));
+    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_CNT_7", rc_handle->rc_stick_ranges[6].center));
+    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_CNT_8", rc_handle->rc_stick_ranges[7].center));
+
+    printf("RC: Committing min/max/center into NVS ... \n");
+    ESP_ERROR_CHECK(nvs_commit(my_handle));
+
+    // Close
+    nvs_close(my_handle);
+	return ESP_OK;
+}
 /**
  * @brief PPM receiver task
  *
@@ -30,6 +84,10 @@ static void ppm_rx_task(void *pvParameter)
 	rc.idle_threshold = RC_PPM_TIMEOUT_US * (RC_TICK_US);
 
 	ESP_ERROR_CHECK(rc_init(rc_handle));
+	printf("Read Values\n");
+	ESP_ERROR_CHECK(rc_start(rc_handle));
+	ESP_ERROR_CHECK(rc_stop(rc_handle));
+
 	printf("Register Min/Max Values\n");
 	ESP_ERROR_CHECK(rc_start(rc_handle));
 	ESP_ERROR_CHECK(rc_stop(rc_handle));
@@ -42,6 +100,8 @@ static void ppm_rx_task(void *pvParameter)
 		rc_handle->rc_stick_ranges[i].center = rc_handle->rc_stick_ranges[i].value;
 		printf("Channel [%d] range: [%d][%d][%d]\n", i, rc_handle->rc_stick_ranges[i].min, rc_handle->rc_stick_ranges[i].center, rc_handle->rc_stick_ranges[i].max);
 	}
+	ESP_ERROR_CHECK(my_rc_init_flash());
+	ESP_ERROR_CHECK(my_rc_write_flash(rc_handle));
 
 	printf("Bye, bye ...\n");
     vTaskDelete(NULL);
