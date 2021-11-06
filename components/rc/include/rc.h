e3aaf31b771093215809eea4a3f9da9df7a4ff61
andrea.lambruschini <andrea.lambruschini@telecomitalia.it>
2021-11-06 20:04:54 +0100

    Aggiunto protocollo i-bus per fly sky

--- components/rc/include/rc.h
+++ components/rc/include/rc.h
@@ -7,21 +7,29 @@
 
 #ifndef COMPONENTS_RC_INCLUDE_RC_H_
 #define COMPONENTS_RC_INCLUDE_RC_H_
+
 #include <stdint.h>
 #include <esp_err.h>
 
+#if CONFIG_ESP_RC_PROTOCOL_IBUS
+#define RC_PROTOCOL_IBUS
+#elif  CONFIG_ESP_RC_PROTOCOL_PPM
+#define RC_PROTOCOL_PPM
+#endif
 
-#define RC_MAX_CHANNELS 8
+#define RC_MAX_CHANNELS 10
 
 typedef enum {
 	RC_THROTTLE = 0,
 	RC_ROLL,
 	RC_PITCH,
 	RC_YAW,
-	RC_AUX1,
-	RC_AUX2,
-	RC_AUX3,
-	RC_AUX4
+	RC_VRA,
+	RC_VRB,
+	RC_SWA,
+	RC_SWB,
+	RC_SWC,
+	RC_SWD
 } RC_STICK;
 
 typedef union {
@@ -31,10 +39,12 @@ typedef union {
 		uint8_t roll;
 		uint8_t pitch;
 		uint8_t yaw;
-		uint8_t aux1;
-		uint8_t aux2;
-		uint8_t aux3;
-		uint8_t aux4;
+		uint8_t vra;
+		uint8_t vrb;
+		uint8_t swa;
+		uint8_t swb;
+		uint8_t swc;
+		uint8_t swd;
 	} fields;
 } rc_stick_to_channel_t;
 
@@ -46,19 +56,14 @@ typedef struct {
 } rc_stick_range_t;
 
 typedef struct {
-    uint8_t channel;
-    uint8_t gpio_num;
-    uint8_t clock_div;
-    uint8_t mem_block_num;
-    uint8_t ticks_thresh;
-    uint16_t idle_threshold;
+	void* dev_config;
+    esp_err_t (*init)(void* rc_handle);
+    esp_err_t (*start)(void* rc_handle);
+    esp_err_t (*stop)(void* rc_handle);
     rc_stick_to_channel_t stick_to_channel;
-	rc_stick_range_t rc_stick_ranges[RC_MAX_CHANNELS];
+    rc_stick_range_t rc_stick_ranges[RC_MAX_CHANNELS];
 } rc_t;
 typedef rc_t* rc_handle_t;
 
-esp_err_t rc_init(rc_handle_t rc_handle);
-esp_err_t rc_start(rc_handle_t rc_handle);
-esp_err_t rc_stop(rc_handle_t rc_handle);
 
 #endif /* COMPONENTS_RC_INCLUDE_RC_H_ */
--- main/main.c
+++ main/main.c
@@ -5,114 +5,31 @@
 #include <nvs.h>
 #include "driver/rmt.h"
 #include "driver/gpio.h"
-#include "rc.h"
+#if CONFIG_ESP_RC_PROTOCOL_IBUS
+#include <rc_ibus.h>
+#include <driver/uart.h>
+#elif  CONFIG_ESP_RC_PROTOCOL_PPM
+#include <rc_ppm.h>
+#endif
+#include "rc_task.h"
 
-#define RC_CHANNEL            0                  /* Channel input (0-7) for receiver */
-#define RC_GPIO_NUM           23                 /* GPIO number for receiver */
-#define RC_CLK_DIV            8                  /* Clock divider */
-#define RC_TICK_US            (80/RC_CLK_DIV)    /* Number of Ticks for us */
-#define RC_PPM_TIMEOUT_US     3500               /* min PPM silence (us) */
-#define RC_BUFF_BLOCK_NUM     4                  /* Memory Blocks */
-#define RC_TICK_TRASH         100                /* Interference */
-
-
-
-esp_err_t my_rc_init_flash(){
-    esp_err_t err = nvs_flash_init();
-    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-        // NVS partition was truncated and needs to be erased
-        // Retry nvs_flash_init
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        err = nvs_flash_init();
-    }
-	return err;
-}
-esp_err_t my_rc_write_flash(rc_handle_t rc_handle){
-    nvs_handle_t my_handle;
-    ESP_ERROR_CHECK(nvs_open("RC_CAL", NVS_READWRITE, &my_handle));
-    ESP_ERROR_CHECK(nvs_set_u8(my_handle, "FLASHED", 1));
-
-    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MIN_1", rc_handle->rc_stick_ranges[0].min));
-    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MIN_2", rc_handle->rc_stick_ranges[1].min));
-    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MIN_3", rc_handle->rc_stick_ranges[2].min));
-    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MIN_4", rc_handle->rc_stick_ranges[3].min));
-    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MIN_5", rc_handle->rc_stick_ranges[4].min));
-    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MIN_6", rc_handle->rc_stick_ranges[5].min));
-    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MIN_7", rc_handle->rc_stick_ranges[6].min));
-    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MIN_8", rc_handle->rc_stick_ranges[7].min));
-
-    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MAX_1", rc_handle->rc_stick_ranges[0].max));
-    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MAX_2", rc_handle->rc_stick_ranges[1].max));
-    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MAX_3", rc_handle->rc_stick_ranges[2].max));
-    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MAX_4", rc_handle->rc_stick_ranges[3].max));
-    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MAX_5", rc_handle->rc_stick_ranges[4].max));
-    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MAX_6", rc_handle->rc_stick_ranges[5].max));
-    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MAX_7", rc_handle->rc_stick_ranges[6].max));
-    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_MAX_8", rc_handle->rc_stick_ranges[7].max));
-
-    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_CNT_1", rc_handle->rc_stick_ranges[0].center));
-    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_CNT_2", rc_handle->rc_stick_ranges[1].center));
-    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_CNT_3", rc_handle->rc_stick_ranges[2].center));
-    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_CNT_4", rc_handle->rc_stick_ranges[3].center));
-    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_CNT_5", rc_handle->rc_stick_ranges[4].center));
-    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_CNT_6", rc_handle->rc_stick_ranges[5].center));
-    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_CNT_7", rc_handle->rc_stick_ranges[6].center));
-    ESP_ERROR_CHECK(nvs_set_u16(my_handle, "RC_CNT_8", rc_handle->rc_stick_ranges[7].center));
-
-    printf("RC: Committing min/max/center into NVS ... \n");
-    ESP_ERROR_CHECK(nvs_commit(my_handle));
-
-    // Close
-    nvs_close(my_handle);
-	return ESP_OK;
-}
-/**
- * @brief PPM receiver task
- *
- */
-static void ppm_rx_task(void *pvParameter)
-{
-	rc_t rc;
-	rc_handle_t rc_handle = &rc;
-
-	rc.gpio_num = RC_GPIO_NUM;
-	rc.channel = RC_CHANNEL;
-	rc.gpio_num = RC_GPIO_NUM;
-	rc.clock_div = RC_CLK_DIV;
-	rc.mem_block_num = RC_BUFF_BLOCK_NUM;
-	rc.ticks_thresh = RC_TICK_TRASH;
-	rc.idle_threshold = RC_PPM_TIMEOUT_US * (RC_TICK_US);
-
-	ESP_ERROR_CHECK(rc_init(rc_handle));
-	printf("Read Values\n");
-	ESP_ERROR_CHECK(rc_start(rc_handle));
-	ESP_ERROR_CHECK(rc_stop(rc_handle));
-
-        vTaskDelay(pdMS_TO_TICKS(20000));
-	printf("Register Min/Max Values\n");
-	ESP_ERROR_CHECK(rc_start(rc_handle));
-	ESP_ERROR_CHECK(rc_stop(rc_handle));
-
-        vTaskDelay(pdMS_TO_TICKS(20000));
-	printf("Register Center Values\n");
-	ESP_ERROR_CHECK(rc_start(rc_handle));
-	ESP_ERROR_CHECK(rc_stop(rc_handle));
-	printf("Min/Center/Max values for Sticks\n");
-	for(uint8_t i = 0; i < RC_MAX_CHANNELS; i++) {
-		rc_handle->rc_stick_ranges[i].center = rc_handle->rc_stick_ranges[i].value;
-		printf("Channel [%d] range: [%d][%d][%d]\n", i, rc_handle->rc_stick_ranges[i].min, rc_handle->rc_stick_ranges[i].center, rc_handle->rc_stick_ranges[i].max);
-	}
-	ESP_ERROR_CHECK(my_rc_init_flash());
-	ESP_ERROR_CHECK(my_rc_write_flash(rc_handle));
-
-	printf("Bye, bye ...\n");
-    vTaskDelete(NULL);
-}
-
+static rc_t rc = {
+#ifdef RC_PROTOCOL_IBUS
+	.init = (esp_err_t (*)(void *))rc_ibus_init,
+	.start = (esp_err_t (*)(void *))rc_ibus_start,
+	.stop = (esp_err_t (*)(void *))rc_ibus_stop
+#else
+#ifdef RC_PROTOCOL_PPM
+	.init = (esp_err_t (*)(void *))rc_ppm_init,
+	.start = (esp_err_t (*)(void *))rc_ppm_start,
+	.stop = (esp_err_t (*)(void *))rc_ppm_stop,
+#endif
+#endif
+};
 void app_main(void)
 {
-    xTaskCreate(ppm_rx_task, "ppm_rx_task", 6144, NULL, 10, NULL);
-
+	rc_handle_t rc_handle = &rc;
+    xTaskCreate(rc_rx_task, "rc_rx_task", 6144, (void*)rc_handle, 10, NULL);
 }
 
 
